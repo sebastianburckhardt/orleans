c5906d77903604a455a955b28451d19d1a74ae88
Jakub Konecki <jkonecki@mwam.com>
2015-12-23 10:34:22 +0000

    Added Event Store interfaces

---
+++ src/OrleansEventSourcing/IEventStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.EventSourcing
+{
+    public interface IEventStore
+    {
+        Task<IEnumerable<object>> LoadStream(string streamId);
+
+        Task<IEnumerable<object>> LoadStreamFromVersion(string streamId, int version);
+
+        Task AppendToStream(string streamId, IEnumerable<object> events);
+    }
+
+    public interface ISnapshot
+    {
+        int AsOfVersion { get; }
+        object Payload { get; }
+    }
+
+    public interface ISupportSnapshots
+    {
+        Task<ISnapshot> LoadLatest(string streamId);
+
+        Task Create(string streamId, int version, object payload);
+    }
+}
