fb3303117cce4d5813aa70455ec29cee39b20388
Sebastian Burckhardt <sburckha@live.com>
2016-03-10 20:03:46 -0800

    begin new sample for replicated events

---
+++ geo/Samples/ReplicatedEventSample/QueuedGrainSample.Grains/HelloGrain.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ReplicatedEventSample.Interfaces;
+using Orleans;
+using Orleans.Providers;
+using Orleans.QueuedGrains;
+using Orleans.Concurrency;
+
+
+namespace ReplicatedEventSample.Grains
+{
+    [Serializable]
+    public class SampleGrainState
+    {
+        public List<string> Messages { get; set; }
+
+        public SampleGrainState()
+        {
+            Messages = new List<string>();
+        }
+    }
+
+    [Serializable]
+    public class AppendMessage : IUpdateOperation<SampleGrainState>
+    {
+        public string Message { get; set; }
+
+        public void Update(SampleGrainState state)
+        {
+            state.Messages.Add(Message);
+        }
+        public override string ToString()
+        {
+            return string.Format("[AppendMessage \"{0}\"]", Message);
+        }
+    }
+
+    [Serializable]
+    public class ClearAll : IUpdateOperation<SampleGrainState>
+    {
+        
+        public void Update(SampleGrainState state)
+        {
+            state.Messages.Clear();
+        }
+        public override string ToString()
+        {
+            return string.Format("[ClearAll]");
+        }
+    }
+
+    [StorageProvider(ProviderName = "GloballySharedAzureAccount")]
+    public class HelloGrain : QueuedGrain<SampleGrainState>, IEventGrain
+    {
+        public Task<LocalState> AppendMessage(string msg)
+        {
+            EnqueueUpdate(new AppendMessage() { Message = msg });
+            return GetLocalState();
+        }
+
+        public Task<LocalState> ClearAll()
+        {
+            EnqueueUpdate(new ClearAll());
+            return GetLocalState();
+        }
+
+        public Task<LocalState> GetLocalState()
+        {
+            return Task.FromResult(new LocalState()
+            {
+                TentativeState = this.TentativeState.Messages,
+                ConfirmedState = this.ConfirmedState.Messages,
+                UnconfirmedUpdates = this.UnconfirmedUpdates.Select((u) => u.ToString()).ToList()
+            });
+        }
+    }
+}
---
+++ geo/Samples/ReplicatedEventSample/QueuedGrainSample.Interfaces/IHelloGrainInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Concurrency;
+
+namespace ReplicatedEventSample.Interfaces
+{
+    public interface IEventGrain : IGrainWithIntegerKey
+    {
+        Task NewOutcome(string name, int rank);
+
+        Task<List<string>> GetTopThree();
+
+        Task<int> GetRank(string name);
+    }
+
+  
+}
